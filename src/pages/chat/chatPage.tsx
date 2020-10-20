1f823afdcc346d35d1f1e2437ea37cd7ff45920b
Zhihao Hong <rayhong118@gmail.com>
2020-10-19 22:13:12 -0700

    remove message query limit

--- src/pages/chat/chatPage.tsx
+++ src/pages/chat/chatPage.tsx
@@ -12,7 +12,7 @@ export const ChatPageComponent = () => {
   const [user, isAuthLoading, authError] = useAuthState(firebase.auth());
   const firestore = firebase.firestore();
   const roomRef = firestore.collection(selectedRoom);
-  const query = roomRef.orderBy("createdAt").limit(6);
+  const query = roomRef.orderBy("createdAt");
 
   const [messages, loading, error] = useCollectionData<ChatMessage>(query, {
     idField: "id",
