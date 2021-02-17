d5ad365dbd4e7cdd409804e4526f2876c9cae901
doghead <rayhong118@gmail.com>
2021-02-16 21:56:51 -0800

    added auth using github

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import firebase from "firebase/app";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { useStateValue } from "../../shared/tools/state";
 
 const AuthPage = () => {
   const [user, isAuthLoading, error] = useAuthState(firebase.auth());
+  const [{ theme }, dispatch] = useStateValue();
+
   const AuthWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  };
+
+  const AuthWithGitHub = () => {
+    const provider = new firebase.auth.GithubAuthProvider();
     firebase.auth().signInWithPopup(provider);
   };
   const signOut = () => {
@@ -14,16 +24,30 @@ const AuthPage = () => {
 
   if (!user)
     return (
-      <div className="page">
-        <button onClick={AuthWithGoogle}>Auth with google</button>
+      <div className="page auth-page">
+        <div className="auth-container">
+          <button onClick={AuthWithGoogle} className="auth-button">
+            <FontAwesomeIcon icon={faGoogle} />
+            Authenticate with Google
+          </button>
+          <button onClick={AuthWithGitHub} className="auth-button">
+            <FontAwesomeIcon icon={faGithub} />
+            Authenticate with GitHub
+          </button>
+        </div>
       </div>
     );
   else
     return (
-      <div className="page">
-        {user?.email}
-        <br />
-        <button onClick={signOut}>Sign out</button>
+      <div className="page auth-page">
+        <div className="auth-container">
+          <img className="profile-img" src="{user.photoURL}" />
+          {user?.email}
+          <br />
+          <button className="sign-out-button" onClick={signOut}>
+            Sign out
+          </button>
+        </div>
       </div>
     );
 };
--- src/shared/components/notValidated/index.tsx
+++ src/shared/components/notValidated/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import "./notValidated.scss";
 
 const NotValidated = () => {
-  return <div>In order to access the content on this page, please sign in</div>;
+  return (
+    <div id="notValidated">
+      In order to access the content on this page, please sign in
+    </div>
+  );
 };
 export default NotValidated;
