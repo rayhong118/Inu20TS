9b0805b0e966466fb04e157ad5d9245e295aff63
doghead <rayhong118@gmail.com>
2021-03-14 16:53:16 -0700

    work in progress banner

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Loading from "../../shared/components/loading";
+import WIP from "../../shared/components/wip";
 import "./home.scss";
 
 const HomePageComponent = () => {
   return (
     <div className="page">
+      <WIP />
       <p>HomePage</p>
       <p>
         This personal site is built without using UI libraries (e.g. Boorstrap,
---
+++ src/shared/components/wip/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./wip.scss";
+
+const WIP = () => {
+  return (
+    <div className="wip">
+      <h2>Work in Progess</h2>
+    </div>
+  );
+};
+
+export default WIP;
